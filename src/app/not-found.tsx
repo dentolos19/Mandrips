368957332a59ad9f14252d946672e69454633db0
Dennise Catolos <dentolos19@gmail.com>
2023-06-30 19:44:40 +0800

    Fixed an issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,7 @@
-import styles from "@/styles/common.scss";
-
-import { makeUseStyles } from "@/lib/utilities";
-
 export default function Page() {
-  const useStyles = makeUseStyles(styles);
   return (
     <main>
-      <div className={useStyles(["navigation-gutter"])}></div>
+      <div className={"navigation-gutter"}></div>
       <div style={{ textAlign: "center" }}>
         <h3>404</h3>
         <p>The page that you're looking for does not exist.</p>
